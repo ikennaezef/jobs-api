f36cc7853cd68ad0f144974692cafd5eeaed14c3
Ikenna <iykee0333@gmail.com>
2023-07-07 15:14:37 +0100

    user model

---
+++ models/User.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+
+const userSchema = new mongoose.Schema({
+	name: {
+		type: String,
+		required: [true, "Please provide a name!"],
+	},
+	email: {
+		type: String,
+		required: [true, "Please provide an email!"],
+		match: [
+			/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+			"Please provide a valid email!",
+		],
+		unique: true,
+	},
+	password: {
+		type: String,
+		required: [true, "Please provide a password!"],
+	},
+});
+
+userSchema.methods.createJWT = function () {
+	return jwt.sign({ id: this._id, name: this.name }, process.env.JWT_SECRET, {
+		expiresIn: process.env.JWT_EXPIRY,
+	});
+};
+
+module.exports = mongoose.model("User", userSchema);
