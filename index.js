b3e4c51213c170ed3bdfee3cd4935c08df166023
Ikenna <iykee0333@gmail.com>
2023-07-13 10:35:45 +0100

    setup security measures

--- index.js
+++ index.js
@@ -6,16 +6,35 @@ const authRoutes = require("./routes/authRoutes");
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 const connectDb = require("./db/connect");
+const validateToken = require("./middleware/auth");
+
+// Security
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimit = require("express-rate-limit");
 
 const app = express();
+
+const limiter = rateLimit({
+	max: 100,
+});
+
+app.set("trust proxy", 1);
+app.use(limiter);
 app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 const port = 5000;
 
+// Homepage
 app.get("/", (req, res) => {
 	res.send("<h1>Jobs API</h1>");
 });
 
-app.use("/api/v1/jobs/", jobsRoutes);
+// Routes
+app.use("/api/v1/jobs/", validateToken, jobsRoutes);
 app.use("/api/v1/auth/", authRoutes);
 
 app.use(errorHandlerMiddleware);
