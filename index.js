b88585307addd20d84004810a0327607f654b655
Ikenna <iykee0333@gmail.com>
2023-07-13 12:45:27 +0100

    docs

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ const helmet = require("helmet");
 const cors = require("cors");
 const xss = require("xss-clean");
 const rateLimit = require("express-rate-limit");
+const port = process.env.PORT || 3001;
 
 const app = express();
 
@@ -26,12 +27,17 @@ app.use(express.json());
 app.use(helmet());
 app.use(cors());
 app.use(xss());
-const port = process.env.PORT || 3001;
+
+// Swagger
+const SwaggerUI = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocs = YAML.load("./swagger.yaml");
 
 // Homepage
 app.get("/", (req, res) => {
-	res.send("<h1>Jobs API</h1>");
+	res.send('<h1>Jobs API</h1><a href="/docs">Documentation<a/>');
 });
+app.use("/docs", SwaggerUI.serve, SwaggerUI.setup(swaggerDocs));
 
 // Routes
 app.use("/api/v1/jobs/", validateToken, jobsRoutes);
